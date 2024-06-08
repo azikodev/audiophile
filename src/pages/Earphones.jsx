e9aed7c79ec72fc77e5afc95f482b919996e40e2
Azizbek <148605374+azikodev@users.noreply.github.com>
2024-06-08 10:02:55 +0500

    Update Earphones.jsx
    audiophile without firabase and signin

--- src/pages/Earphones.jsx
+++ src/pages/Earphones.jsx
@@ -53,7 +53,7 @@ function Earphones() {
                           {product.description}
                         </p>
                         <button className="text-white hover:bg-[#FBAF85] w-40 h-12 bg-[#D87D4A] border-none uppercase">
-                          <Link to="/">See product</Link>
+                          <Link to={`/products/${product.slug}`}>See product</Link>
                         </button>
                       </div>
                       {product.id == 6 ? (
